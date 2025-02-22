95cbbf515ef176df7557c54f0771394cc61a90b0
Beatriz Dile <bedos-sa@student.42sp.org.br>
2025-02-22 17:05:25 -0300

    feat: rendering chat history

--- backend/apps/chat/views.py
+++ backend/apps/chat/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Message, Chat, ChatParticipants
+from apps.users.models import User
 from django.utils import timezone
 
 @login_required
@@ -9,20 +11,27 @@ def index(request):
 
 @login_required
 def create_room(request, room_uuid, room_name):
-    if Chat.objects.filter(name=room_name).exists():
-        return render(request, "chat/room_already_exists.html", {"room_name": room_name})
-
     chat = Chat.objects.create(id=room_uuid, name=room_name, is_group_chat=True)
     ChatParticipants.objects.create(chat=chat, user=request.user, joined_at=timezone.now())
 
-    print(f"CREATED ROOM: room_name: {room_name} room_uuid: {room_uuid}")
-
-    return render(request, "chat/room.html", {"room_uuid": room_uuid})
+    return redirect("enter_room", room_uuid=room_uuid)
 
 @login_required
 def enter_room(request, room_uuid):
-    if not Chat.objects.filter(id=room_uuid).exists():
-        return render(request, "chat/room_not_found.html", {"room_uuid": room_uuid})
+    chat = get_object_or_404(Chat, id=room_uuid)
+    messages = Message.objects.filter(chat=chat).order_by("sent_at")
+    messages_list = list(messages.values("sender", "content", "sent_at"))
+    sender = list(messages.values("sender"))
+    
+    for i in range(len(sender)):
+        sender[i] = User.objects.get(id=sender[i]["sender"]).username
 
-    print(f"FOUND ROOM: room_uuid: {room_uuid}")
-    return render(request, "chat/room.html", {"room_uuid": room_uuid})
+    for i in range(len(messages_list)):
+        messages_list[i]["sender"] = sender[i]
+
+    context = {
+        "chat": chat,
+        "messages": messages_list,
+    }
+
+    return render(request, "chat/room.html", context)
