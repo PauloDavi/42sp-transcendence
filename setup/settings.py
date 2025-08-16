f71607308594f763f180b8cea7f06ff02fb1d900
Paulo Davi <paulo.araujo@cobli.co>
2025-08-16 11:34:35 -0300

    fix: redirect url

--- setup/settings.py
+++ setup/settings.py
@@ -1,4 +1,3 @@
-
 import os
 from pathlib import Path
 import dj_database_url
@@ -28,7 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'whitenoise.runserver_nostatic',    
+    "whitenoise.runserver_nostatic",
     "django_extensions",
     "allauth",
     "allauth.account",
@@ -115,9 +114,7 @@ import dj_database_url
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL:
-    DATABASES = {
-        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
-    }
+    DATABASES = {"default": dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)}
 else:
     DATABASES = {
         "default": {
@@ -166,15 +163,12 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "setup/static"),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Media files
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -189,8 +183,6 @@ MESSAGE_TAGS = {
 }
 
 LOGIN_URL = "login"
-CSRF_TRUSTED_ORIGINS = [
-    "https://transcendence.up.railway.app"
-]
+CSRF_TRUSTED_ORIGINS = ["https://transcendence.up.railway.app"]
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_CREDENTIALS = True
--- setup/urls.py
+++ setup/urls.py
@@ -1,20 +1,18 @@
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
-from django.conf.urls.static import static
+from django.views.static import serve
 from django.contrib import admin
 from django.urls import include, path, re_path
 
 
-urlpatterns = (
-    i18n_patterns(
-        re_path(r"^rosetta/", include("rosetta.urls")),
-        path("i18n/", include("django.conf.urls.i18n")),
-        path("admin/", admin.site.urls),
-        path("", include("apps.users.urls")),
-        path("matchmaking/", include("apps.matchmaking.urls")),
-        path("chat/", include("apps.chat.urls")),
-    )
-    + [
-        path("accounts/", include("apps.users.providers.fortytwo.urls")),
-    ]
-)
+urlpatterns = i18n_patterns(
+    re_path(r"^rosetta/", include("rosetta.urls")),
+    path("i18n/", include("django.conf.urls.i18n")),
+    path("admin/", admin.site.urls),
+    path("", include("apps.users.urls")),
+    path("matchmaking/", include("apps.matchmaking.urls")),
+    path("chat/", include("apps.chat.urls")),
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+) + [
+    path("accounts/", include("apps.users.providers.fortytwo.urls")),
+]
