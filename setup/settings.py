bed18c3d9a9c85a54bd377f48de1238a4b84d910
Paulo Davi <paulo.araujo@cobli.co>
2025-08-16 09:50:26 -0300

    feat: fix app volume

--- setup/settings.py
+++ setup/settings.py
@@ -172,7 +172,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media files
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join("/app", "media")
 MEDIA_URL = "/media/"
 
 # Default primary key field type
